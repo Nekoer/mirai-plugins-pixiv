e0a8b091fdbd4985e157022235a877bd51511ea0
EvolvedGhost <727511070@qq.com>
2024-02-23 01:01:12 +0800

    更新依赖

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    val kotlinVersion = "1.6.21"
+    val kotlinVersion = "1.9.20"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
 
 
     id("org.jetbrains.kotlin.plugin.noarg") version kotlinVersion
     id("org.jetbrains.kotlin.plugin.allopen") version kotlinVersion
-    id("net.mamoe.mirai-console") version "2.15.0-M1"
+    id("net.mamoe.mirai-console") version "2.16.0"
     id("me.him188.maven-central-publish") version "1.0.0-dev-3"
 }
 
@@ -17,18 +17,18 @@ version = "1.7.6"
 
 repositories {
 //    mavenLocal()
-//    maven("https://maven.aliyun.com/repository/gradle-plugin")
-//    maven("https://maven.aliyun.com/repository/central")
+    maven("https://maven.aliyun.com/repository/central")
+    maven("https://maven.aliyun.com/repository/gradle-plugin")
     mavenCentral()
 }
 dependencies {
-    implementation("org.apache.commons:commons-lang3:3.12.0")
+    implementation("org.apache.commons:commons-lang3:3.14.0")
     implementation("commons-codec:commons-codec:1.15")
-    implementation("org.apache.httpcomponents:httpclient:4.5.13")
-    implementation("org.jsoup:jsoup:1.15.3")
-    implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.10")
+    implementation("org.apache.httpcomponents:httpclient:4.5.14")
+    implementation("org.jsoup:jsoup:1.15.4")
+    implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.12")
     implementation("com.madgag:animated-gif-lib:1.4")
-    compileOnly("org.bytedeco:javacv-platform:1.5.7")
+    compileOnly("org.bytedeco:javacv-platform:1.5.10")
 //    compileOnly
     implementation(kotlin("stdlib-jdk8"))
 }
